5bc89140e34dcbc1004e65c2d7423eb332894aea
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2021-03-26 19:05:25 -0700

    did weird algorithm and added fence painting

---
+++ Guide/General/Fence Painting/cmake-build-debug/CMakeFiles/Fence_Painting.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fence_Painting.dir/main.cpp.obj"
+  "Fence_Painting.exe"
+  "Fence_Painting.exe.manifest"
+  "Fence_Painting.pdb"
+  "libFence_Painting.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fence_Painting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Guide/General/WeirdAlgorithm/cmake-build-debug/CMakeFiles/WeirdAlgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WeirdAlgorithm.dir/main.cpp.obj"
+  "WeirdAlgorithm.exe"
+  "WeirdAlgorithm.exe.manifest"
+  "WeirdAlgorithm.pdb"
+  "libWeirdAlgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WeirdAlgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
